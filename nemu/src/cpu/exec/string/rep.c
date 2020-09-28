0d9f3ac0607e1567a797c0b79f35971639b6abc5
tracer-ics2016 <tracer@njuics.org>
2020-09-28 14:13:28 +0000

    >  compile NEMU 3018210133 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:13:28 up 2 days,  8:54,  1 user,  load average: 1.22, 0.81, 0.61 33623f8d21e1e9749700c26c70967430858ac2ea

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -15,7 +15,7 @@ make_helper(rep) {
 			exec(eip + 1);
 			count ++;
 			cpu.ecx --;
-			/*assert(ops_decoded.opcode == 0xa4	// movsb
+			assert(ops_decoded.opcode == 0xa4	// movsb
 				|| ops_decoded.opcode == 0xa5	// movsw
 				|| ops_decoded.opcode == 0xaa	// stosb
 				|| ops_decoded.opcode == 0xab	// stosw
@@ -23,16 +23,11 @@ make_helper(rep) {
 				|| ops_decoded.opcode == 0xa7	// cmpsw
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
-				);*/
+				);
 
-			if ((ops_decoded.opcode == 0xa6	
-				|| ops_decoded.opcode == 0xa7	
-				|| ops_decoded.opcode == 0xae	
-				|| ops_decoded.opcode == 0xaf) && cpu.ZF == 0)break;
-
-		}
 			/* TODO: Jump out of the while loop if necessary. */
 
+		}
 		len = 1;
 	}
 
@@ -51,20 +46,16 @@ make_helper(repnz) {
 		exec(eip + 1);
 		count ++;
 		cpu.ecx --;
-		/*assert(ops_decoded.opcode == 0xa6	// cmpsb
+		assert(ops_decoded.opcode == 0xa6	// cmpsb
 				|| ops_decoded.opcode == 0xa7	// cmpsw
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
-			  );*/
-		if ((ops_decoded.opcode == 0xa6	
-				|| ops_decoded.opcode == 0xa7	
-				|| ops_decoded.opcode == 0xae	
-				|| ops_decoded.opcode == 0xaf) && cpu.ZF == 1)break;
-
-	}
+			  );
 
 		/* TODO: Jump out of the while loop if necessary. */
 
+
+	}
 
 #ifdef DEBUG
 	char temp[80];
